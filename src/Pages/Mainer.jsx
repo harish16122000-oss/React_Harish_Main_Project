cc36f1b7959abe3ffe3acbaf49976ffedb70f6ef
harish16122000-oss <harish16122000@gmail.com>
2026-04-13 07:31:16 +0530

    18

--- src/Pages/Mainer.jsx
+++ src/Pages/Mainer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Wheat from '../Components/Wheat'
 import Rice from '../Components/Rice'
+import Millet from '../Components/Millet'
 
 const Mainer = ({Mybutton}) => {
   return (
     <div>
      <Wheat Mybutton={Mybutton}/>
+     <Millet Mybutton={Mybutton}/>
      <Rice Mybutton={Mybutton}/>   
     </div>
   )
